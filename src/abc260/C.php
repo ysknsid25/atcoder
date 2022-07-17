6a9115aabbeae4293a665462e9aec4acfe47f430
Ramen Tabetarou <kobeengineer1225@gmail.com>
2022-07-18 07:46:53 +0900

    abc260 C: これは簡単
    Signed-off-by: Ramen Tabetarou <kobeengineer1225@gmail.com>

--- src/abc260/C.php
+++ src/abc260/C.php
@@ -12,42 +12,12 @@ for ($i = 1; $i < 11; $i++) {
 
 $Rarr[$N] = 1;
 
-while (true) {
-  $doRExchange = true;
-  foreach ($Rarr as $lv => $cnt) {
-    if ($cnt == 0) {
-      continue;
-    }
-    if ($lv > 1) {
-      $doRExchange = $doRExchange && ($cnt > 0);
-      $nextlv = $lv - 1;
-      for ($i = 0; $i < $cnt; $i++) {
-        $Rarr[$lv] -= 1;
-        $Rarr[$nextlv] += 1;
-        $Barr[$lv] += $X;
-      }
-    }
-  }
-  $doBExchange = true;
-  foreach ($Barr as $lv => $cnt) {
-    if ($cnt == 0) {
-      continue;
-    }
-    if ($lv > 1) {
-      $doBExchange = $doBExchange && ($cnt > 0);
-      $nextlv = $lv - 1;
-      for ($i = 0; $i < $cnt; $i++) {
-        $Barr[$lv] -= 1;
-        $Rarr[$nextlv] += 1;
-        $Barr[$nextlv] += $Y;
-      }
-    }
-  }
-  if (!$doRExchange && !$doBExchange) {
-    break;
-  }
-  var_dump($Rarr);
-  var_dump($Barr);
-  break;
+for ($i = $N; $i >= 2; $i--) {
+  $Rarr[$i - 1] += $Rarr[$i];
+  $Barr[$i] += $Rarr[$i] * $X;
+
+  $Rarr[$i - 1] += $Barr[$i];
+  $Barr[$i - 1] += $Barr[$i] * $Y;
 }
+
 echo $Barr[1];
