7f15dd8dfc309fd8c926ea6ca2df1ccfac9cf15d
Ramen Tabetarou <kobeengineer1225@gmail.com>
2022-07-30 07:31:08 +0900

    abc243 A
    Signed-off-by: Ramen Tabetarou <kobeengineer1225@gmail.com>

---
+++ src/abc243/A.py
@@ -0,0 +1,13 @@
+v, a, b, c = map(int, input().split())
+
+people = [a, b, c]
+who = {0: "F", 1: "M", 2: "T"}
+remain = v
+cnt = 0
+while(True):
+    i = cnt % 3
+    remain = remain - people[i]
+    if(remain < 0):
+        print(who[i])
+        break
+    cnt += 1
