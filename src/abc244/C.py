f5b15f5c19dee3e9b42f23c1bcc9816055979b55
Ramen Tabetarou <kobeengineer1225@gmail.com>
2022-08-12 08:03:28 +0900

    tmp
    Signed-off-by: Ramen Tabetarou <kobeengineer1225@gmail.com>

---
+++ src/abc244/C.py
@@ -0,0 +1,13 @@
+def main():
+    N = int(input())
+    rem = set(range(1, 2*N+2))
+    while True:
+        print(rem.pop(), flush=True)
+        a = int(input())
+        if a == 0:
+            break
+        rem.discard(a)
+
+
+if __name__ == 'main':
+    main()
