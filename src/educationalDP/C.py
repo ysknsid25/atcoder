347fd952cbfe7841a0ba43e1d7d6e13a200a24c1
ysknsid25 <ng_knok@docomo.ne.jp>
2022-09-04 18:08:38 +0900

    educational DP C
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- src/educationalDP/C.py
+++ src/educationalDP/C.py
@@ -1,25 +1,10 @@
-def getInputFile():
-    f = open('/Users/yoshidakengo/project/atcoder/src/sample.txt', 'r')
-    datalist = f.readlines()
-    f.close()
-    return datalist
-
-
-def removeReturncd(str):
-    return str.rstrip('\n')
-
-
-inputs = getInputFile()
-
-# ここから入れ替える
-
-n = int(inputs[0])
+n = int(input())
 
 dp = [[0 for i in range(3)] for i in range(n + 1)]
 
 hp = []
 for i in range(0, n):
-  tmp = list(map(int, inputs[i+1].split()))
+  tmp = list(map(int, input().split()))
   hp.append(tmp)
 
 for i in range(0, n):
@@ -30,7 +15,6 @@ for i in range(0, n):
       else:
         dp[i+1][k] = max(dp[i+1][k], dp[i][j] + hp[i][k])
 
-print(dp)
 ans = 0
 for i in range(3):
   ans = max(ans, dp[n][i])
