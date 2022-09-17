d5b5bf9740f89296c39c12b6f1f67e82d451da1c
ysknsid25 <ng_knok@docomo.ne.jp>
2022-09-18 07:55:15 +0900

    abc269 C
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- src/abc260-269/abc269/C.py
+++ src/abc260-269/abc269/C.py
@@ -1,9 +1,14 @@
-n = int(input())
-binn = format(n, 'b')
+# 2進数aに対する部分集合の列挙を取得する典型パターン
+# 7行目からが肝
 
-for i in range(n+1):
-  bin2 = format(i, 'b')
-  binand = binn & bin2
-  if bin2 == binand:
-    print(i)
-    
+n = int(input())
+ans = []
+s = n
+while(True):
+  ans.append(s)
+  if s == 0: break
+  s = (s - 1) & n
+
+ans.sort()
+for i in ans:
+  print(i)
