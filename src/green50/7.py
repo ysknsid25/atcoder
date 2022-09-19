23ec03f5520f1de6199fc748375d2ebb90b991db
ysknsid25 <ng_knok@docomo.ne.jp>
2022-09-19 10:53:38 +0900

    7 AC
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- src/green50/7.py
+++ src/green50/7.py
@@ -11,42 +11,38 @@ s = []
 for i in range(h):
   r = list(input())
   s.append(r)
-  
-maxcansee = 0
-for r in range(h):
-  see = 0
-  for c in range(w):
-    see = cansee(r, c, s)
-    if see == 0:
-      continue
-    # 左
-    l = c-1
-    while(l > -1):
-      tmp = cansee(r, l, s)
-      if tmp == 0:
-        break
-      see += 1
-      l -= 1
-    # 右
-    for ri in range(c+1, w):
-      tmp = cansee(r, ri, s)
-      if tmp == 0:
-        break
-      see += 1
-    # 下
-    d = r-1
-    while(d > -1):
-      tmp = cansee(d, c, s)
-      if tmp == 0:
-        break
-      see += 1
-      d -= 1
-    # 上
-    for u in range(r+1, h):
-      tmp = cansee(u, c, s)
-      if tmp == 0:
-        break
-      see += 1
-    maxcansee = max(maxcansee, see)
 
-print(maxcansee)
+r = x-1
+c = y-1
+see = 0
+see = cansee(r, c, s)
+# 左
+l = c-1
+while(l > -1):
+  tmp = cansee(r, l, s)
+  if tmp == 0:
+    break
+  see += 1
+  l -= 1
+# 右
+for ri in range(c+1, w):
+  tmp = cansee(r, ri, s)
+  if tmp == 0:
+    break
+  see += 1
+# 下
+d = r-1
+while(d > -1):
+  tmp = cansee(d, c, s)
+  if tmp == 0:
+    break
+  see += 1
+  d -= 1
+# 上
+for u in range(r+1, h):
+  tmp = cansee(u, c, s)
+  if tmp == 0:
+    break
+  see += 1
+
+print(see)
