cf55c875761cd3561a1c1f23fe544edd6c793f1a
ysknsid25 <kengo071225@gmail.com>
2022-10-09 06:50:41 +0900

    abc204 C
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/green50/27.py
+++ src/green50/27.py
@@ -1,22 +1,29 @@
+# 重要例題 BFS
 from collections import deque
 
-# 重要例題 BFS
+def bfscity(start, n, connect):
+  visited = [False] * (n+1)
+  visited[start] = True
+  cnt = 1
+  que = deque()
+  que.append(start)
+  while len(que) > 0:
+    nowcity = que.popleft()
+    for tocity in connect[nowcity]:
+      if not visited[tocity]:
+        visited[tocity] = True
+        cnt += 1
+        que.append(tocity)
+  return cnt
+
 n, m = map(int, input().split())
-g = [[] for i in range(n)]
+connect = [[] for i in range(n+1)]
 
 for i in range(m):
   a, b = map(int, input().split())
-  # 配列のindexに合わせる
-  a -= 1
-  b -= 1
+  connect[a].append(b)
 
-  tmp = g[a]
-  tmp.append(b)
-  g[a] = tmp
-
-starts = [i for i in range(n)]
-for city in starts:
-  count = 1
-  visited = deque()
-  visited.appendleft(city)
-    
+ans = 0
+for i in range(1, n+1):
+  ans += bfscity(i, n, connect)
+print(ans)
