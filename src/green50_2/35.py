ac0237ffba5986d8f7c7c9112eca2a8078166bc1
ysknsid25 <kengo071225@blessingsoftware.jp>
2022-11-04 06:30:07 +0900

    abc146 C
    Signed-off-by: ysknsid25 <kengo071225@blessingsoftware.jp>

--- src/green50_2/35.py
+++ src/green50_2/35.py
@@ -1,60 +1,26 @@
-"""
-Union Find
-"""
-class UnionFind:
-  def __init__(self, n):
-    self.n = n
-    self.parent_size=[-1] * n
+def price(n,a,b):
+  strn = str(n)
+  d = len(strn)
+  p = a*n + b*d
+  return p
 
-  """
-  リーダーが同じなら何もしない
-  リーダーが違う場合、グループの人数が少ない方を、大きい方のグループへ統合する
-  """
-  def merge(self, a, b):
-    x, y = self.leader(a), self.leader(b)
-    if x == y:
-      return
-    if abs(self.parent_size[x]) < abs(self.parent_size[y]):
-      x, y = y, x
-    self.parent_size[x] += self.parent_size[y]
-    self.parent_size[y] = x
-    return
-  
-  def same(self, a, b):
-    return self.leader[a] == self.leader[b]
+a,b,x = map(int, input().split())
 
-  """
-  再帰で根にいるリーダーを探しに行く
-  自分自身がリーダーなら自分を返す
-  """
-  def leader(self, a):
-    if self.parent_size[a] < 0:
-      return a
-    self.parent_size[a] = self.leader(self.parent_size[a])
-    return self.parent_size[a]
+if x<price(1,a,b):
+  print(0)
+  exit()
 
-  """
-  aが所属するグループのサイズ(人数)を返す
-  """
-  def size(self, a):
-    return abs(self.parent_size[self.leader(a)])
-
-  def groups(self):
-    result = [[] for _ in range(self.n)]
-    for i in range(self.n):
-      result[self.leader(i)].append(i)
-    return [r for r in result if r != []]
-
-n,m = map(int, input().split())
-uftree = UnionFind(n)
-for i in range(m):
-  a,b = map(int, input().split())
-  a -= 1
-  b -= 1
-  uftree.merge(a,b)
-
-groups = uftree.groups()
+left = 1
+right = 10 ** 10
 ans = 0
-for group in groups:
-  ans = max(ans, len(group))
+while 1 < right - left:
+  mid = (left + right)//2
+  if price(mid,a,b) <= x:
+    left = mid
+  else:
+    right = mid
+  ans = left
+
+if 10**9 < ans:
+  ans = 10**9
 print(ans)
