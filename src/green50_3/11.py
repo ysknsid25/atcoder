bb2f8920e4e0fbc623752284d125c25f91fa19a7
ysknsid25 <kengo071225@blessingsoftware.jp>
2022-11-06 17:25:17 +0900

    abc203 C
    Signed-off-by: ysknsid25 <kengo071225@blessingsoftware.jp>

---
+++ src/green50_3/11.py
@@ -0,0 +1,18 @@
+n,k = map(int, input().split())
+f = []
+for i in range(n):
+  a,b = map(int, input().split())
+  f.append([a,b])
+f.sort()
+
+now = 0
+now += k
+for i in range(n):
+  fv = f[i][0]
+  fm = f[i][1]
+  if fv <= now:
+    now += fm
+  else:
+    break
+
+print(now)
