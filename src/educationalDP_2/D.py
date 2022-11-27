c5e2069ac40761f7c49df14d3fbdb8e1c4d2b7cf
ysknsid25 <kengo071225@gmail.com>
2022-11-27 18:02:23 +0900

    EDPC D
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ src/educationalDP_2/D.py
@@ -0,0 +1,17 @@
+n,w=map(int,input().split())
+_INF = 10**20*-1
+dp = [[_INF for i in range(w+1)] for i in range(n+1)]
+dp[0][0] = 0
+for i in range(1,n+1):
+  iw,iv = map(int,input().split())
+  for j in range(0,w+1):
+    if j >= iw:
+      #! i番目の品物を選ぶ前の時点の重さと価値 + i番目を選んだ時っていうのを舐めていく
+      #! 一つ前の品物を選んだ時点でj-iwが0未満の場合はおかしいので選べない
+      dp[i][j] = max(dp[i-1][j],dp[i-1][j-iw]+iv)
+    else:
+      dp[i][j] = dp[i-1][j]
+ans = 0
+for i in range(w+1):
+  ans = max(ans, dp[n][i])
+print(ans)
