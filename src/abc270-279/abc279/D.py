58961abb5fc9d9ca51a448e2d03b13158de7ab4b
ysknsid25 <kengo071225@gmail.com>
2022-11-28 20:24:46 +0900

    abc279 D
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/abc270-279/abc279/D.py
+++ src/abc270-279/abc279/D.py
@@ -1,13 +1,29 @@
+"""
+三分探索
+・とある時点までは減少し、最小値を迎えたあとは増加する場合
+・とある時点までは増加し、最大値を迎えたあとは減少する場合
+この二つのパターンにおいて効果を発揮する。
+
+三分探索の基本的な形はどれも同じ
+"""
+
 import math
+def speed(a,b,x):
+  g = x+1
+  res = b*x+a/math.sqrt(g)
+  return res
+
 a,b=map(int,input().split())
-ans = 10**20
-notupd = 0
-g = 0
-while notupd < 3:
-  time = (b*g) + (a/math.sqrt(g+1))
-  if time < ans:
-    ans = time
+ans = -1
+l=0
+r=10**18
+while l+3 <= r:
+  c1 = (l*2+r)//3
+  c2 = (l+r*2)//3
+  if speed(a,b,c1) >= speed(a,b,c2):
+    l = c1
   else:
-    notupd += 1
-  g+=1
+    r = c2
+
+ans = min(speed(a,b,l),speed(a,b,l+1),speed(a,b,r))
 print(ans)
