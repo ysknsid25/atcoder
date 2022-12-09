d330a57d91507cc3ce5b8dac295cc52c67ee86b5
ysknsid25 <kengo071225@gmail.com>
2022-12-09 10:02:52 +0900

    abc185 F
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/common.py
+++ src/common.py
@@ -255,3 +255,56 @@ def nCr_mod(n, r, mod=10**9+7):
         de %= mod
     de_inv = pow(de, -1, mod)
     return nu*de_inv % mod
+
+#! セグメントツリー
+"""
+演算を行う。必要に応じて変更する
+"""
+def segfunc(x,y):
+    return x^y
+
+class SegTree:
+    def __init__(self,x_list,init,segfunc):
+        self.init=init
+        self.segfunc=segfunc
+        self.Height=len(x_list).bit_length()+1
+        self.Tree=[init]*(2**self.Height)
+        self.num=2**(self.Height-1)
+        for i in range(len(x_list)):
+            self.Tree[2**(self.Height-1)+i]=x_list[i]
+        for i in range(2**(self.Height-1)-1,0,-1):
+            self.Tree[i]=segfunc(self.Tree[2*i],self.Tree[2*i+1])
+
+        """指定のインデックスの値を返す
+        """
+    def select(self,k):
+        return self.Tree[k+self.num]
+
+        """指定のインデックスの値を更新する
+        """
+    def update(self,k,x):
+        i=k+self.num
+        self.Tree[i]=x
+        while i>1:
+            if i%2==0:
+                self.Tree[i//2]=self.segfunc(self.Tree[i],self.Tree[i+1])
+            else:
+                self.Tree[i//2]=self.segfunc(self.Tree[i-1],self.Tree[i])
+            i//=2
+
+        """指定区間についての演算結果を返す
+        """
+    def query(self,l,r):
+        result=self.init
+        l+=self.num
+        r+=self.num+1
+
+        while l<r:
+            if l%2==1:
+                result=self.segfunc(result,self.Tree[l])
+                l+=1
+            if r%2==1:
+                result=self.segfunc(result,self.Tree[r-1])
+            l//=2
+            r//=2
+        return result
