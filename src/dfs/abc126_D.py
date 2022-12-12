b7fa3e80ec2a642b9753a6d68ab94afd3229ad6e
ysknsid25 <kengo071225@gmail.com>
2022-12-12 19:58:43 +0900

    abc126 D WA
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/dfs/abc126_D.py
+++ src/dfs/abc126_D.py
@@ -1,9 +1,27 @@
 n=int(input())
 con=[[] for i in range(n)]
-line=[[] for i in range(n)] for i in range(n)
-for i in range(n):
-  fr,to,w=,map(int,input().split())
-  con[fr].append(to)
-  con[to].append(fr)
-  line[fr][to]=w
-  line[to][fr]=w
+v=set()
+for i in range(n-1):
+  fr,to,w=map(int,input().split())
+  fr-=1
+  to-=1
+  con[fr].append([to,w])
+  con[to].append([fr,w])
+  v.add(fr)
+  v.add(to)
+seen=[-1 for i in range(len(list(v)))]
+
+def dfs(seen,g,v,c):
+    seen[v]=c
+    for to,w in g[v]:
+        if seen[to]!=-1:
+            continue
+        if w%2==1:
+          c=1-c
+        #! 再帰的に探索
+        dfs(seen,g,to,c)
+    return seen
+
+seen=dfs(seen,con,0,0)
+for c in seen:
+  print(c)
