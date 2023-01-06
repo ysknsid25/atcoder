169f171c4552a14da8d13452de65a76a13dfcdd9
ysknsid25 <kengo071225@gmail.com>
2023-01-06 14:46:10 +0900

    abc211 D
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/greenTypical/37.py
+++ src/greenTypical/37.py
@@ -12,27 +12,26 @@ _TIME=0
 _ROUTE=1
 _INF=10**20
 visited=[[_INF,0] for i in range(n+1)]
+visited[1][_TIME]=0
+visited[1][_ROUTE]=1
 que=deque()
 que.append(1)
 while len(que)>0:
   now=que.popleft()
   for nxt in con[now]:
+    now_time,now_route = visited[now]
     nxt_time,nxt_route = visited[nxt]
     if nxt_time==_INF:
-      visited[nxt][_TIME]=1
-      visited[nxt][_ROUTE]=1
+      visited[nxt][_TIME]=now_time+1
+      visited[nxt][_ROUTE]=now_route
       que.append(nxt)
     else:
-      now_time,now_route = visited[now]
       if now_time+1==nxt_time:
-        visited[nxt][_TIME]=now_time+1
-        visited[nxt][_ROUTE]=now_route+1
-        que.append(nxt)
-      elif now_time+1<nxt_time:
-        visited[nxt][_TIME]=now_time+1
-        visited[nxt][_ROUTE]=1
-        que.append(nxt)
+        visited[nxt][_ROUTE]+=now_route
+    visited[nxt][_ROUTE]%=10**9+7
 
 route=visited[n][1]
-ans=route%(10**9+7)
-print(ans)
+if route==_INF:
+  print(0)
+else:
+  print(route)
