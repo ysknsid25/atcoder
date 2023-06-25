b3c6293d87a33324f21d74cd4487e9e46889396b
ysknsid25 <kengo071225@gmail.com>
2023-06-25 17:57:37 +0900

    abc203 C
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ src/2023/abc/c/abc203.py
@@ -0,0 +1,16 @@
+n,k=map(int,input().split())
+friends=[[] for i in range(n)]
+for i in range(n):
+    a,b=map(int,input().split())
+    friends[i]=[a,b]
+
+friends.sort()
+
+now=k
+for i in range(n):
+    v=friends[i][0]
+    if v<=now:
+        now+=friends[i][1]
+    else:
+        break
+print(now)
