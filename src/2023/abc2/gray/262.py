33656bab6026d067ff88ded7d37d5dda4607d954
ysknsid25 <kengo071225@gmail.com>
2023-10-16 09:39:57 +0900

    ABC262 C
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/2023/abc2/gray/262.py
+++ src/2023/abc2/gray/262.py
@@ -1,10 +1,19 @@
-from collections import defaultdict
 n=int(input())
-smap=defaultdict(int)
-for i in range(n):
-    s=input()
-    if smap[s]>0:
-        print(s+"("+str(smap[s])+")")
-    else:
-        print(s)
-    smap[s]+=1
+a=[0]+list(map(int,input().split()))
+
+ans=0
+
+# ai=iになる要素がいくつあるかを探す。その中から二つ選んだ時の組み合わせが答えの一部
+x=0
+for i in range(1,n+1):
+    if a[i]==i:
+        x+=1
+ans+=x*(x-1)//2
+
+# ai=jになる要素がいくつあるか探す。a[ai]=iとなり、なおかつi<jであるもの
+for i in range(1,n+1):
+    j=a[i]
+    if a[j]==i and i<j:
+        ans+=1
+
+print(ans)
