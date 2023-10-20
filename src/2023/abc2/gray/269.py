1dfcccf71f7572f511c06201ad4a8b18ba803871
ysknsid25 <kengo071225@gmail.com>
2023-10-20 09:32:49 +0900

    ABC269 C
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ src/2023/abc2/gray/269.py
@@ -0,0 +1,14 @@
+# 2進数aに対する部分集合の列挙を取得する典型パターン
+# 7行目からが肝
+
+n = int(input())
+ans = []
+s = n
+while(True):
+    ans.append(s)
+    if s == 0: break
+    s = (s - 1) & n
+
+ans.sort()
+for i in ans:
+    print(i)
